ba1df000813a87c11eb2ebc1e2e1b71a27530d9e
Jim O'Donnell <jim@zooniverse.org>
2019-07-17 13:07:23 +0100

    Simple NextJS app Set up a simple app with NextJS and Express. AWS credentials are read from process.env. Show a simple text email form. On submit, post the contents of the form to Amazon SES.

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+const Index = () => (
+  <form
+    method="post"
+    action="/contact"
+  >
+    <fieldset>
+      <legend>your email</legend>
+      <label>From <input type="text" name="from" defaultValue="" /></label>
+      <label>To <input type="text" name="to" defaultValue="" /></label>
+      <label>
+        Body text
+        <textarea
+          name="emailBody"
+        >
+        </textarea>
+      </label>
+    </fieldset>
+    <button
+      type="submit"
+    >
+      Send
+    </button>
+  </form>
+);
+
+export default Index;
---
+++ server/server.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const next = require('next')
+const bodyParser = require('body-parser')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+const AWS = require('aws-sdk')
+
+AWS.config.update({
+  accessKeyId: process.env.AMAZON_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AMAZON_SECRET_ACCESS_KEY,
+  region: 'us-east-1'
+});
+
+const ses = new AWS.SES({ apiVersion: "2010-12-01" })
+const params = {
+  Destination: {
+    ToAddresses: [] // Email address/addresses that you want to send your email
+  },
+  Message: {
+    Body: {
+      Text: {
+        Charset: "UTF-8",
+        Data: ""
+      }
+    },
+    Subject: {
+      Charset: "UTF-8",
+      Data: "Test email"
+    }
+  },
+  Source: ""
+};
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.use(bodyParser.urlencoded({ extended: true }))
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.post('/contact', (req, res) => {
+    const { emailBody, from, to } = req.body
+    params.Source = from
+    params.Destination.ToAddresses.push(to)
+    params.Message.Body.Text.Data = emailBody
+    const sendEmail = ses.sendEmail(params).promise()
+    sendEmail
+      .then(function emailSent(data) {
+        console.log(data)
+        res.send(`Email sent! ${data}`)
+      })
+      .catch(function emailFailed(error) {
+        console.log(error)
+        res.send(error.message)
+      })
+  })
+
+  server.listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Read on http://localhost:3000')
+  })
+})
